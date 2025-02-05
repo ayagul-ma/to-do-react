dd3836e6edcf5b753a1ab7c32d26b05d46ba1a94
24LP0954 24LP0954 <24LP0954@G303-CC0148.local>
2025-02-05 13:00:34 +0800

    react

---
+++ src/app/components/input/index.js
@@ -0,0 +1,11 @@
+const Input = () => {
+    return (
+     <div>
+        <input placeholder="Add a new task.."/>
+        <button>Add</button>
+     </div>
+    )
+      
+    
+}
+export default Input
---
+++ src/app/components/title/index.js
@@ -0,0 +1,6 @@
+const Title = () => {
+    return <h1>To-Do list</h1>
+      
+    
+}
+export default Title
